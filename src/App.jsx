7f1b4f5820809ee80da765f05caf7d05a2b959ac
Diana Monteiro <dianar.monteiro@gmail.com>
2026-06-11 16:35:09 -0300

    add ajustes e bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/index.jsx";
 import Home from "./pages/Home/home.jsx";
-import PrivateRoute from "./components/Header/privateRoute.jsx";
-import Header from "./components/Header/index.jsx";
+import Navbar from "./components/NavBar/index.jsx";
+import PrivateRoute from "./routes/privateRoute.jsx";
 
 function App() {
   return (
     <>
-      <Header />
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,58 @@
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  const navLinks = [
+    { id: 1, label: "Início", path: "/" },
+    { id: 2, label: "Cadastrar Livro", path: "/cadastrar" },
+  ];
+
+  return (
+    <nav
+      className={`navbar navbar-expand-lg bg-body-tertiary ${styles.navbarCustom}`}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          📚 Biblioteca
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {/* O map mágico acontecendo aqui */}
+            {navLinks.map((link) => (
+              <li className="nav-item" key={link.id}>
+                <a className="nav-link" href={link.path}>
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          {/* Mantivemos a barra de pesquisa que será perfeita para buscar um livro específico */}
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Buscar livro..."
+              aria-label="Buscar"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Buscar
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
