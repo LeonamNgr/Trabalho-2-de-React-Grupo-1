228fcd3d2df3f3a0088f9b6ced069e52e12ff98e
LEONAM NOGUEIRA MACHADO <leonamngr@gmail.com>
2026-06-11 16:16:07 -0300

    Atualizacao

--- src/service/api.js
+++ src/service/api.js
@@ -1,37 +1,49 @@
 import axios from 'axios';
 
-// Configuração base da API
+
 const api = axios.create({
   baseURL: 'http://localhost:8080' 
 });
 
-// Buscar um livro específico (GET)
-export const buscarLivroPorId = async (id) => {
-  const response = await api.get(`/livros/${id}`);
-  return response.data;
-};
 
-// Buscar todos os livros (GET)
+api.interceptors.request.use((config) => {
+ 
+  const token = localStorage.getItem('token'); 
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+
+
+
 export const buscarTodosOsLivros = async () => {
-  const response = await api.get('/livros');
-  return response.data; // Vai devolver a lista/array com todos os livros
+  const response = await api.get('/Livro/todos-os-livros'); 
+  return response.data;
 };
 
-// Criar um novo livro (POST)
+export const buscarLivroPorId = async (id) => {
+  const response = await api.get(`/Livro/${id}`);
+  return response.data;
+};
+
 export const criarLivro = async (dadosDoLivro) => {
-  const response = await api.post('/livros', dadosDoLivro);
+  const response = await api.post('/Livro/adicionar-livro', dadosDoLivro);
   return response.data;
 };
 
-// Atualizar um livro existente (PUT)
 export const atualizarLivro = async (id, dadosDoLivro) => {
-  const response = await api.put(`/livros/${id}`, dadosDoLivro);
+  const response = await api.put(`/Livro/atualizar-livro/${id}`, dadosDoLivro);
   return response.data;
 };
 
-// Deletar um livro (DELETE)
 export const deletarLivro = async (id) => {
-  const response = await api.delete(`/livros/${id}`);
+  const response = await api.delete(`/Livro/deletar-livro/${id}`);
   return response.data;
 };
 
